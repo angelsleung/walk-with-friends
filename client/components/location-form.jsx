11a4085f2b2c1e11c1d6bbb19db6be98b576ef74
Angel Leung <angelhleung@gmail.com>
2021-04-06 16:08:55 -0700

    Added a React form element with three text inputs for the locations, labels for the text inputs, and a 'Go' button. #1

--- client/components/location-form.jsx
+++ client/components/location-form.jsx
@@ -4,23 +4,35 @@ export default class LocationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { start: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  // handleChange(event) {
 
-  }
+  // }
 
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  // }
 
   render() {
     return (
       <form>
-        <label htmlFor="start">Start (A)</label>
-        <input type="text" value={this.state.start} />
+        <h1>Map a Route</h1>
+        <div>
+          <label htmlFor="start">Start (A)</label>
+          <input type="text" value={this.state.startA} />
+        </div>
+        <div>
+          <label htmlFor="start">Stop (B)</label>
+          <input type="text" value={this.state.stopB} />
+        </div>
+        <div>
+          <label htmlFor="start">Stop (C)</label>
+          <input type="text" value={this.state.stopC} />
+        </div>
+        <button>Go</button>
       </form>
     );
   }
