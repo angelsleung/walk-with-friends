19997050d53cc6b8b4e78a82692f14bd030a6860
Angel Leung <angelhleung@gmail.com>
2021-04-07 21:45:06 -0700

    Removed all styling that had no effect or was otherwise unnecessary. #1

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
 
-export default class MapComponent extends React.Component {
+export default class Map extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { start: '' };
     this.mapRef = React.createRef();
     this.mapInstance = null;
   }
 
   componentDidMount() {
-    const loader = new Loader({
-      apiKey: 'AIzaSyCG4ySJPtAS0a5rJRHZV8uPM6fgOzCZD0A'
-    });
-
-    loader.load().then(() => {
-      this.mapInstance = new google.maps.Map(this.mapRef.current,
-        {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8
-        }
-      );
-    });
   }
 
   render() {
