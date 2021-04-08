7dd4d6eda8c6ae2ac2fe8aa19ff6ea3d1880ade9
Angel Leung <angelhleung@gmail.com>
2021-04-07 22:00:30 -0700

    Fixed all errors. #1

--- client/components/location-form.jsx
+++ client/components/location-form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Loader } from '@googlemaps/js-api-loader';
+
 export default class LocationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -21,53 +21,48 @@ export default class LocationForm extends React.Component {
   }
 
   componentDidMount() {
-    const loader = new Loader({
-      apiKey: 'AIzaSyCG4ySJPtAS0a5rJRHZV8uPM6fgOzCZD0A',
-      libraries: ['places']
+    this.autocompleteInstanceA = new google.maps.places.Autocomplete(
+      this.autocompleteRefA.current,
+      {
+        types: ['establishment'],
+        componentRestrictions: { country: 'US' },
+        fields: ['place_id', 'geometry', 'name']
+      }
+    );
+    this.autocompleteInstanceB = new google.maps.places.Autocomplete(
+      this.autocompleteRefB.current,
+      {
+        types: ['establishment'],
+        componentRestrictions: { country: 'US' },
+        fields: ['place_id', 'geometry', 'name']
+      }
+    );
+    this.autocompleteInstanceC = new google.maps.places.Autocomplete(
+      this.autocompleteRefC.current,
+      {
+        types: ['establishment'],
+        componentRestrictions: { country: 'US' },
+        fields: ['place_id', 'geometry', 'name']
+      }
+    );
+
+    this.autocompleteInstanceA.addListener('place_changed', () => {
+      const place = this.autocompleteInstanceA.getPlace();
+      if (place.geometry) {
+        this.setState({ A: place.name });
+      }
     });
-    loader.load().then(() => {
-      this.autocompleteInstanceA = new google.maps.places.Autocomplete(
-        this.autocompleteRefA.current,
-        {
-          types: ['establishment'],
-          componentRestrictions: { country: 'US' },
-          fields: ['place_id', 'geometry', 'name']
-        }
-      );
-      this.autocompleteInstanceB = new google.maps.places.Autocomplete(
-        this.autocompleteRefB.current,
-        {
-          types: ['establishment'],
-          componentRestrictions: { country: 'US' },
-          fields: ['place_id', 'geometry', 'name']
-        }
-      );
-      this.autocompleteInstanceC = new google.maps.places.Autocomplete(
-        this.autocompleteRefC.current,
-        {
-          types: ['establishment'],
-          componentRestrictions: { country: 'US' },
-          fields: ['place_id', 'geometry', 'name']
-        }
-      );
-      this.autocompleteInstanceA.addListener('place_changed', () => {
-        const place = this.autocompleteInstanceA.getPlace();
-        if (place.geometry) {
-          this.setState({ A: place.name });
-        }
-      });
-      this.autocompleteInstanceB.addListener('place_changed', () => {
-        const place = this.autocompleteInstanceB.getPlace();
-        if (place.geometry) {
-          this.setState({ B: place.name });
-        }
-      });
-      this.autocompleteInstanceC.addListener('place_changed', () => {
-        const place = this.autocompleteInstanceC.getPlace();
-        if (place.geometry) {
-          this.setState({ C: place.name });
-        }
-      });
+    this.autocompleteInstanceB.addListener('place_changed', () => {
+      const place = this.autocompleteInstanceB.getPlace();
+      if (place.geometry) {
+        this.setState({ B: place.name });
+      }
+    });
+    this.autocompleteInstanceC.addListener('place_changed', () => {
+      const place = this.autocompleteInstanceC.getPlace();
+      if (place.geometry) {
+        this.setState({ C: place.name });
+      }
     });
   }
 
@@ -94,17 +89,20 @@ export default class LocationForm extends React.Component {
         <div className="input-div">
           <label htmlFor="startA" className="location-label">Start (A)</label>
           <input type="text" value={this.state.A} onChange={this.handleChangeA}
-            className="location-input" ref={this.autocompleteRefA} id="startA" />
+            className="location-input" ref={this.autocompleteRefA} id="startA"
+            required={true} />
         </div>
         <div className="input-div">
           <label htmlFor="stopB" className="location-label">Stop (B)</label>
           <input type="text" value={this.state.B} onChange={this.handleChangeB}
-            className="location-input" ref={this.autocompleteRefB} id="stopB" />
+            className="location-input" ref={this.autocompleteRefB} id="stopB"
+            required={true} />
         </div>
         <div className="input-div">
           <label htmlFor="stopC" className="location-label">Stop (C)</label>
           <input type="text" value={this.state.C} onChange={this.handleChangeC}
-            className="location-input" ref={this.autocompleteRefC} id="stopC" />
+            className="location-input" ref={this.autocompleteRefC} id="stopC"
+            required={true} />
         </div>
         <div className="input-div">
           <label className="location-label">End (A)</label>
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { Loader } from '@googlemaps/js-api-loader';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+const loader = new Loader({
+  apiKey: 'AIzaSyCG4ySJPtAS0a5rJRHZV8uPM6fgOzCZD0A',
+  libraries: ['places']
+});
+
+loader.load().then(() => {
+  ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+  );
+});
