667cbd22adc3d52574198ee91123bc8401686a42
Angel Leung <angelhleung@gmail.com>
2021-04-09 01:19:56 -0700

    Ensured that the map appears when the user clicks the submit button on the location form, and fixed all errors. #2

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/header';
-// import MapRoute from './pages/map-route';
 import LocationForm from './components/location-form';
 import Map from './components/map';
 import AppContext from './lib/app-context';
