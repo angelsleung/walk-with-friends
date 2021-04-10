225189b477be0ebf18de1f90b28f7166ce3d6627
Angel Leung <angelhleung@gmail.com>
2021-04-10 16:57:40 -0700

    Updated index.js to determine how the server will respond to the GET request. #4

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
 app.use(staticMiddleware);
 
+const jsonMiddleware = express.json();
+
+app.use(jsonMiddleware);
+
+app.get('/api/routes', (req, res) => {
+  const sql = `
+    select *
+    from "routes"
+    order by "createdAt"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'an unexpected error occurred'
+      });
+    });
+});
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
