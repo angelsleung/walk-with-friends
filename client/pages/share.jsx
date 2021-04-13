cbfd43c9b5e04b8e52b3b8d19d8cfa585a5e1cde
Angel Leung <angelhleung@gmail.com>
2021-04-12 21:33:57 -0700

    Adjusted CSS styling to ensure that all pages look fine on different mobile screen sizes. #5

--- client/pages/share.jsx
+++ client/pages/share.jsx
@@ -6,6 +6,24 @@ export default class Share extends React.Component {
       <div className="page">
         <h1 className="page-title">Select Friend</h1>
         <div className="friend-list">
+          <div className="friend-list-item">
+            <input type="checkbox" className="checkbox" id="friend"></input>
+            <label className="friend-label" htmlFor="friend">
+              <i className="friend-icon fas fa-user-circle"></i>
+              <p className="friend-name">Misty</p>
+            </label>
+          </div>
+          <div className="friend-list-item">
+            <input type="checkbox" className="checkbox" id="friend"></input>
+            <label className="friend-label" htmlFor="friend">
+              <i className="friend-icon fas fa-user-circle"></i>
+              <p className="friend-name">Brock</p>
+            </label>
+          </div>
+        </div>
+        <div className="add-date-div">
+          <i className="add-date-icon fas fa-calendar-plus"></i>
+          <p className="add-date-text">Add a date</p>
         </div>
         <div className="center input-div">
           <button className="button">Share</button>
