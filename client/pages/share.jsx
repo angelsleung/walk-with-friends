1040effa458f226d9f8eddf3ce8ca793d64e7ddf
Angel Leung <angelhleung@gmail.com>
2021-04-12 22:11:06 -0700

    Wrote a fetch request in share.jsx to obtain a list of the user's friends from the database. #5

--- client/pages/share.jsx
+++ client/pages/share.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 
 export default class Share extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      friends: []
+    };
+  }
+
+  getFriends() {
+    fetch('/api/friends')
+      .then(res => res.json())
+      .then(friends => this.setState({ friends }));
+  }
+
+  renderFriends() {
+    return (
+      this.state.friends.map((friend, index) => {
+        return (
+          <div key={index} className="friend-list-item">
+            <input type="checkbox" className="checkbox" id={index} name="friend"></input>
+            <label className="friend-label" htmlFor={index}>
+              <i className="friend-icon fas fa-user-circle"></i>
+              <p className="friend-name">{friend}</p>
+            </label>
+          </div>
+        );
+      })
+    );
+  }
+
   render() {
     return (
       <div className="page">
         <h1 className="page-title">Select Friend</h1>
         <div className="friend-list">
-          <div className="friend-list-item">
-            <input type="checkbox" className="checkbox" id="1" name="friend"></input>
-            <label className="friend-label" htmlFor="1">
-              <i className="friend-icon fas fa-user-circle"></i>
-              <p className="friend-name">Misty</p>
-            </label>
-          </div>
-          <div className="friend-list-item">
-            <input type="checkbox" className="checkbox" id="2" name="friend"></input>
-            <label className="friend-label" htmlFor="2">
-              <i className="friend-icon fas fa-user-circle"></i>
-              <p className="friend-name">Brock</p>
-            </label>
-          </div>
+          {this.renderFriends()}
         </div>
         <div className="add-date-div">
           <i className="add-date-icon fas fa-calendar-plus"></i>
