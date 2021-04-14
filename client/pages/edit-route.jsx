275f8930abe775bb55f786fafee9c34e2b1476f4
Angel Leung <angelhleung@gmail.com>
2021-04-14 10:37:06 -0700

    Added logos from Font Awesome and set up the page to look like the wireframe using example information. #7

--- client/pages/edit-route.jsx
+++ client/pages/edit-route.jsx
@@ -5,14 +5,27 @@ export default class EditRoute extends React.Component {
     return (
       <div className="page">
         <h1 className="page-title">Edit Route</h1>
-        <div className="friend-list">
-        </div>
-        <div className="add-date-div">
-          <i className="add-date-icon fas fa-calendar-plus" />
-          <p className="add-date-text">Add a date</p>
+        <div className="edit-page">
+          <div className="delete-icons">
+            <i className="fas fa-minus-circle"></i>
+            <i className="fas fa-minus-circle"></i>
+          </div>
+          <div>
+            <div className="edit-section">
+              <h2>Last walked</h2>
+              <p>4/1/21</p>
+            </div>
+            <div className="edit-section">
+              <h2>Next walk</h2>
+              <p>4/17/21 @ 2:00pm</p>
+            </div>
+            <div className="edit-section">
+              <span className="delete-route">Delete Route</span>
+            </div>
+          </div>
         </div>
         <div className="center input-div">
-          <button className="button">Share</button>
+          <button className="button">Done</button>
         </div>
       </div>
     );
