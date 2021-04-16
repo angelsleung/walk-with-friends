0f435ca27e3a9cdba4b59e4c3284e2cb89859309
Angel Leung <angelhleung@gmail.com>
2021-04-15 21:18:52 -0700

    Structure the app to look like the wireframe, using dummy data. #9

--- client/pages/friends-routes.jsx
+++ client/pages/friends-routes.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 export default class FriendsRoutes extends React.Component {
+  renderRoutes() {
+    return (
+      <div className="route-list-item">
+        <div className="route-icon-details">
+          <i className="shoes-icon fas fa-shoe-prints"></i>
+          <div className="route-item-details">
+            <h2>{"Misty's Route"}</h2>
+            <p>Saturday 4/3/21</p>
+            <p>11:00am</p>
+          </div>
+        </div>
+        <div className="route-item-totals">
+          <div className="route-item-distance">0.7 mi</div>
+          <div className="route-item-duration">15 mins</div>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="page">
         <h1 className="page-title">{"Friends' Routes"}</h1>
         <div className="route-list">
+          {this.renderRoutes()}
         </div>
       </div>
     );
