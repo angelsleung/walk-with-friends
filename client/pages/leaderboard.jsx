b060248361c9b81f2d7a9a38cb1df9b6c887112f
Angel Leung <angelhleung@gmail.com>
2021-04-16 11:48:23 -0700

    Set up the page layout using dummy data so that the page looks like the wireframe. #10

--- client/pages/leaderboard.jsx
+++ client/pages/leaderboard.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 export default class Leaderboard extends React.Component {
+  renderFriends() {
+    return (
+      <div className="friend-item">
+        <div className="friend-name-rank">
+          <div className="friend-rank">1</div>
+          <i className="friend-icon fas fa-user-circle"></i>
+          <div className="friend-name">Ash</div>
+        </div>
+        <div className="friend-distance">5.2 mi</div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="page">
         <h1 className="page-title">Friends</h1>
+        <h2 className="week">Sun 4/4 - Sat 4/10</h2>
         <div className="friends-list">
+          {this.renderFriends()}
         </div>
       </div>
     );
