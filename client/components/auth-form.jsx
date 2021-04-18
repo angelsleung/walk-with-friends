a47c3289d0d89f1a2220e4b2cd8d8879b91a85bf
Angel Leung <angelhleung@gmail.com>
2021-04-18 14:09:17 -0700

    Edited CSS styling so that log-in page looks nice on iPhone 5/5S. #21

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 export default class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={this.handleSubmit}>
         <div className="auth-input-div">
-          <input required autoFocus type="text" name="username"
-            className="auth-input" placeholder="Username"></input>
+          <input required autoFocus type="text" name="username" className="auth-input"
+            placeholder="Username" onChange={this.handleChange}></input>
         </div>
         <div className="auth-input-div">
-          <input required type="password" name="password"
-            className="auth-input" placeholder="Password"></input>
+          <input required type="password" name="password" className="auth-input"
+            placeholder="Password" onChange={this.handleChange}></input>
         </div>
         <div className="submit auth-input-div">
           <button type="submit" className="auth-submit">Log In</button>
