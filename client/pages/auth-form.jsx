72b31f39eae1d88376e3ac209b72fd5e3116a4ca
Angel Leung <angelhleung@gmail.com>
2021-04-18 01:28:28 -0700

    Created a new page auth-form.jsx for user login. #21

---
+++ client/pages/auth-form.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class AuthForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="username" className="form-label">Username</label>
+          <input required autoFocus id="username" type="text" name="username"></input>
+        </div>
+        <div>
+          <label htmlFor="password" className="form-label">Password</label>
+          <input required id="password" type="password" name="password"></input>
+        </div>
+        <div>
+          <button type="submit"></button>
+        </div>
+
+      </form>
+    );
+  }
+}
