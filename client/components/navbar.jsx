542ba3dd35aae7c666f9e9f8297948b89fbc090b
Angel Leung <angelhleung@gmail.com>
2021-04-22 13:28:51 -0700

    Made sure that app drawer closes and overlay dissapears upon clicking on the navbar again. #21

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import AppDrawer from './app-drawer';
 
-export default function Navbar(props) {
-  return (
-    <footer className="navbar">
-      <a href="#"><i className="fas fa-home"></i></a>
-      <a href="#map-route"><i className="fas fa-map-marked-alt"></i></a>
-      <a href="#saved-routes"><i className="fas fa-heart"></i></a >
-      <AppDrawer />
-    </footer>
-  );
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.props.isOpen) {
+      this.props.onClick();
+    }
+  }
+
+  render() {
+    return (
+      <footer className="navbar" onClick={this.handleClick}>
+        <a href="#"><i className="fas fa-home"></i></a>
+        <a href="#map-route"><i className="fas fa-map-marked-alt"></i></a>
+        <a href="#saved-routes"><i className="fas fa-heart"></i></a >
+        <AppDrawer />
+      </footer>
+    );
+  }
 }
