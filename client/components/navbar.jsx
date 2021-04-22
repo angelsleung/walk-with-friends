f9f7f1908cefe7dc6f730b5fca23830c588e7cbc
Angel Leung <angelhleung@gmail.com>
2021-04-22 10:39:25 -0700

    Replaced Friends' Routes icon with the app drawer icon on the navbar. #21

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppDrawer from './app-drawer';
 
 export default function Navbar(props) {
   return (
@@ -6,7 +7,7 @@ export default function Navbar(props) {
       <a href="#"><i className="fas fa-home"></i></a>
       <a href="#map-route"><i className="fas fa-map-marked-alt"></i></a>
       <a href="#saved-routes"><i className="fas fa-heart"></i></a >
-      <a href="#friends-routes"><i className="fas fa-user-friends"></i></a >
+      <AppDrawer />
     </footer>
   );
 }
