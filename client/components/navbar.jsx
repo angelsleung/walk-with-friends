1c9ee7df79df8352853547038880bddb7c7d65ad
Angel Leung <angelhleung@gmail.com>
2021-05-05 14:04:52 -0700

    Added text labels to the navbarlinks

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -18,10 +18,33 @@ export default class Navbar extends React.Component {
       <footer>
         <nav className="navbar" onClick={this.handleClick}>
           <ul className="nav-list">
-            <li><a href="#" aria-label="Home"><i className="fas fa-home" /></a></li>
-            <li><a href="#map-route" aria-label="Map Route"><i className="fas fa-map-marked-alt" /></a></li>
-            <li><a href="#saved-routes" aria-label="Saved Routes"><i className="fas fa-heart" /></a></li>
-            <li><AppDrawer aria-label="Menu"/></li>
+            <li>
+              <a href="#" aria-label="Home">
+                <div className="navbar-div">
+                  <i className="fas fa-home" />
+                  <span className="nav-label">home</span>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="#map-route" aria-label="Map Route">
+                <div className="navbar-div">
+                  <i className="fas fa-map-marked-alt" />
+                  <span className="nav-label">new</span>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href="#saved-routes" aria-label="Saved Routes">
+                <div className="navbar-div">
+                  <i className="fas fa-heart" />
+                  <span className="nav-label">saved</span>
+                </div>
+              </a>
+            </li>
+            <li>
+              <AppDrawer aria-label="Menu"/>
+            </li>
           </ul>
         </nav>
       </footer>
